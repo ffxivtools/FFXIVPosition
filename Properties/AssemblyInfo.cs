f2e0bb6e5c6ace482eb8220ca21d2e3528224ebb
ffxivtools <62024883+ffxivtools@users.noreply.github.com>
2020-03-10 10:21:12 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("AstelliaPosition")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AstelliaPosition")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c9d5b835-f66e-4fc3-aecb-f5ed2b24613d")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Properties/Resources.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace FFXIVPosition.Properties
+{
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		internal Resources()
+		{
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new ResourceManager("FFXIVPosition.Properties.Resources", typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		internal static Bitmap dn
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("dn", Resources.resourceCulture);
+			}
+		}
+
+		internal static Bitmap fdn
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("fdn", Resources.resourceCulture);
+			}
+		}
+
+		internal static Bitmap fup
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("fup", Resources.resourceCulture);
+			}
+		}
+
+		internal static Bitmap lt
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("lt", Resources.resourceCulture);
+			}
+		}
+
+		internal static Bitmap rt
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("rt", Resources.resourceCulture);
+			}
+		}
+
+		internal static Bitmap up
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("up", Resources.resourceCulture);
+			}
+		}
+
+		private static ResourceManager resourceMan;
+
+		private static CultureInfo resourceCulture;
+	}
+}
